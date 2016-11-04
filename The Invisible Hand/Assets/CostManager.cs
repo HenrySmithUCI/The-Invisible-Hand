339d6cb6c81df1948676798d7afcbcd905b71421
Henry Smith <hgsmith@uci.edu>
2016-11-03 18:20:14 -0700

    Added buttons to Market Screen

---
+++ The Invisible Hand/Assets/CostManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CostManager : Singleton<CostManager> {
+
+  protected CostManager() { }
+
+  public void addOneResource(string resource) {
+    ResourceStorage.Instance.addResource(resource, 1);
+  }
+}
