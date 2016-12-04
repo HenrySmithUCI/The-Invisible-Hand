b9ece2f30fc32dd606fb346699c5f93505059764
kbuci <krishenb@uci.edu>
2016-12-03 22:55:25 -0800

    last commit didnt work, this should

---
+++ The Invisible Hand/Assets/saveTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class saveTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        savedStatistics profile = new savedStatistics();
+        saveSystem saver = new saveSystem(profile);
+        //saver.SaveData();
+        saver.LoadData();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
